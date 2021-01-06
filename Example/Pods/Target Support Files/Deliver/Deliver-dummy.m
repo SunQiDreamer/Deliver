7c30e20d838498410d4604e779713cfd215b364f
孙琦 <993473797@qq.com>
2021-01-06 20:39:44 +0800

    5

---
+++ Example/Pods/Target Support Files/Deliver/Deliver-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Deliver : NSObject
+@end
+@implementation PodsDummy_Deliver
+@end
---
+++ Example/Pods/Target Support Files/Pods-Deliver_Tests/Pods-Deliver_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Deliver_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Deliver_Tests
+@end
